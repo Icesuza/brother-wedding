b4fe44c4a77dd746ce511cb9bd3f1e61a2c38755
Icesuza <hengliza81@gmail.com>
2026-03-15 19:56:05 +0700

    my first commit

---
+++ app/components/Homepage.tsx
@@ -0,0 +1,12 @@
+export default function Homepage() {
+  return (
+    <>
+      <div className="font-serif text-gold-dark">
+        Hello from the wedding invitation homepage!
+      </div>
+      <div className="font-chenla text-gold-dark">
+        សួស្តីពីទំព័រដើមនៃការអញ្ជើញរៀបការរបស់យើង!
+      </div>
+    </>
+  );
+}
